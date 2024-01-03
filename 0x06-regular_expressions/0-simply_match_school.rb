27360180beebd67f6aee554674e1e5372cf691f3
Naodsh <naodadanech1977@gmail.com>
2024-01-03 07:21:36 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Extract the argument
-text = ARGV[0]
-
-# Define the regular expression
-pattern = /School/
-
-# Match the pattern in the given text
-match = text.match(pattern)
-
-# Output the result
-puts match
+puts ARGV[0].scan(/School/).join
