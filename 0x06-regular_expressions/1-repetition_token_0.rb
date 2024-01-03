276fa1f9587939b936756beb3b7dbf029a3c3d61
Naodsh <naodadanech1977@gmail.com>
2024-01-03 07:27:57 -0800

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <text>"
-  exit 1
-end
-
-# Extract the argument
-text = ARGV[0]
-
-# Define the regular expression with repetition token
-pattern = /hbt{2,5}n/
-
-# Match the pattern in the given text
-match = text.match(pattern)
-
-# Output the result
-puts match ? match[0] : "No match"
+puts ARGV[0].scan(/hbt{2,5}n/).join
