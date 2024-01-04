fe95c8a66cdc57ad6f14f1cd7e873a6fab95555b
Naodsh <naodadanech1977@gmail.com>
2024-01-04 06:09:50 -0800

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-# Define a method to extract information from log entries
-def extract_info(log_entry)
-  sender = log_entry.match(/\[from:([^ \]]+)/)[1]
-  receiver = log_entry.match(/\[to:([^ \]]+)/)[1]
-  flags = log_entry.match(/\[flags:([^ \]]+)/)[1]
-
-  "#{sender},#{receiver},#{flags}"
-end
-
-# Read the log file and process each line
-File.foreach("logfile.txt") do |line|
-  if line.include?("Receive SMS") || line.include?("Sent SMS")
-    puts extract_info(line)
-  end
-end
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:(\+?\w+)\] \[flags:(-?\d:-?\d:-?\d:-?\d:-?\d)\]/).join(",")
