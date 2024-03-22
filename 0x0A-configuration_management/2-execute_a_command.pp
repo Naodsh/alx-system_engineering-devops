909fd57936754c2021178a63efff6a637f1b6694
Naodsh <naodadanech1977@gmail.com>
2024-03-22 04:10:41 -0700

    0x0A

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# This manifest uses 'exec' resource to kill a process named 'killmenow' using 'pkill'
-exec { 'kill_killmenow_process':
-  command => 'pkill -f killmenow',
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-}
