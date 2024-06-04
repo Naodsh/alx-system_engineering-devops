44756febc8c6784d3d20bed6cfe6f087e621c3f1
Naodsh <naodadanech@1977>
2024-06-04 02:59:57 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
+import requests
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    If the subreddit is invalid or an error occurs, returns 0.
     """
-    import requests
-
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "alxRedditBot")
+    headers = {"User-Agent": "alxRedditBot"}
 
     try:
         response = requests.get(url, headers=headers)
