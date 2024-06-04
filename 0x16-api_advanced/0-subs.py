b50b7c399d70aa1bbfc72ad800e55150391ec478
Naodsh <naodadanech@1977>
2024-06-04 03:51:01 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid or an error occurs, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "alxRedditBot"}
 
-    try:
-        response = requests.get(url, headers=headers)
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16.api.advanced )"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
-    except (requests.RequestException, KeyError):
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
