dff41788bef54d610c7277bff30cd0477b8bb2f5
Naodsh <naodadanech@1977>
2024-06-10 09:17:05 -0700

    0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,19 @@
+class nginx_optimization {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    subscribe  => File['/etc/nginx/nginx.conf'],
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    content => template('nginx_optimization/nginx.conf.erb'),
+    notify  => Service['nginx'],
+  }
+}
+
+include nginx_optimization
