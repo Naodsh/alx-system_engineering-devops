6a3e64603806d9362ddfc19106b7d375cd30c06f
Naodsh <naodadanech@1977>
2024-06-14 09:50:44 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,27 +1,41 @@
-# manifests/init.pp
+# sky_is_the_limit_not.pp
+# This Puppet manifest configures Nginx to handle a high number of connections and requests more efficiently
 
-# Class: nginx_optimization
-# This class installs and configures Nginx with optimal settings to handle high loads.
-class nginx_optimization {
-	# Ensure the Nginx package is installed
-	package { 'nginx':
-		ensure => installed,
-		}
-
-	# Ensure the Nginx service is running and enabled
-	service { 'nginx':
-		ensure    => running,
-		enable    => true,
-		subscribe => File['/etc/nginx/nginx.conf'],
-		}
-
-	# Apply optimal Nginx configuration from template
-	file { '/etc/nginx/nginx.conf':
-		ensure  => file,
-		content => template('nginx_optimization/nginx.conf.erb'),
-		notify  => Service['nginx'],
-		}
+exec { 'increase-worker-connections':
+  command => 'sed -i "s/worker_connections .*/worker_connections 1024;/" /etc/nginx/nginx.conf',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  onlyif  => 'grep -q "worker_connections 768;" /etc/nginx/nginx.conf',
+  notify  => Exec['restart-nginx'],
 }
 
-# Apply the nginx_optimization class
-include nginx_optimization
+exec { 'increase-worker-processes':
+  command => 'sed -i "s/worker_processes .*/worker_processes auto;/" /etc/nginx/nginx.conf',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  onlyif  => 'grep -q "^worker_processes" /etc/nginx/nginx.conf',
+  notify  => Exec['restart-nginx'],
+}
+
+exec { 'increase-keepalive-timeout':
+  command => 'sed -i "s/keepalive_timeout .*/keepalive_timeout 65;/" /etc/nginx/nginx.conf',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  onlyif  => 'grep -q "^keepalive_timeout" /etc/nginx/nginx.conf',
+  notify  => Exec['restart-nginx'],
+}
+
+exec { 'increase-server-names-hash-bucket-size':
+  command => 'sed -i "s/server_names_hash_bucket_size .*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+  onlyif  => 'grep -q "^server_names_hash_bucket_size" /etc/nginx/nginx.conf',
+  notify  => Exec['restart-nginx'],
+}
+
+exec { 'restart-nginx':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+}
+
+# Ensure puppet-lint compliance
+exec { 'ensure-puppet-lint-compliance':
+  command => 'puppet-lint 0-the_sky_is_the_limit_not.pp --fix',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
+}
