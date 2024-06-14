91d6d0bc2df18c2886ad836cadb8c781491e40fa
Naodsh <naodadanech@1977>
2024-06-14 06:58:03 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,24 +3,24 @@
 # Class: nginx_optimization
 # This class installs and configures Nginx with optimal settings to handle high loads.
 class nginx_optimization {
-  # Ensure the Nginx package is installed
-  package { 'nginx':
-    ensure => installed,
-  }
+	# Ensure the Nginx package is installed
+	package { 'nginx':
+		ensure => installed,
+		}
 
-  # Ensure the Nginx service is running and enabled
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/nginx/nginx.conf'],
-  }
+	# Ensure the Nginx service is running and enabled
+	service { 'nginx':
+		ensure    => running,
+		enable    => true,
+		subscribe => File['/etc/nginx/nginx.conf'],
+		}
 
-  # Apply optimal Nginx configuration from template
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    content => template('nginx_optimization/nginx.conf.erb'),
-    notify  => Service['nginx'],
-  }
+	# Apply optimal Nginx configuration from template
+	file { '/etc/nginx/nginx.conf':
+		ensure  => file,
+		content => template('nginx_optimization/nginx.conf.erb'),
+		notify  => Service['nginx'],
+		}
 }
 
 # Apply the nginx_optimization class
