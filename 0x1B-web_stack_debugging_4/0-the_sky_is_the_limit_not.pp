e966aad5dbd6f15f95e6105977742822da534c4c
Naodsh <naodadanech@1977>
2024-06-14 03:48:55 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,21 @@
-# webstuck deb
+# manifests/init.pp
+
+# Class: nginx_optimization
+# This class installs and configures Nginx with optimal settings to handle high loads.
 class nginx_optimization {
+  # Ensure the Nginx package is installed
   package { 'nginx':
     ensure => installed,
   }
 
+  # Ensure the Nginx service is running and enabled
   service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    subscribe  => File['/etc/nginx/nginx.conf'],
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/nginx.conf'],
   }
 
+  # Apply optimal Nginx configuration from template
   file { '/etc/nginx/nginx.conf':
     ensure  => file,
     content => template('nginx_optimization/nginx.conf.erb'),
@@ -17,4 +23,5 @@ class nginx_optimization {
   }
 }
 
+# Apply the nginx_optimization class
 include nginx_optimization
