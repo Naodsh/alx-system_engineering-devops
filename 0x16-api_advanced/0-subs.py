a2883d406de3f6b8cb1fbee493a83b64461bb5ef
Naodsh <naodadanech@1977>
2024-06-17 04:08:49 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,6 @@ function returns 0.
 
 import requests
 
-
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given subreddit.
@@ -18,23 +17,13 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': 'reddit-subscriber-counter/0.1 by u/UnfairArtist8484',
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Debugging information
-        print(f"Status Code: {response.status_code}")
-        #print(f"Response Content: {response.text}")
-
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscriber']
-        else:
-            return 0
-    except requests.RequestException as e:
-        print(f"RequestException: {e}")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
